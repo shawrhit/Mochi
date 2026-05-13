b7f221c8a69eb36e0e82bddb30ee038a8e0c61f7
shawrhit <shawrhit@gmail.com>
2026-05-13 17:30:39 +0530

    Added new animations

--- src/animation_catalog.cpp
+++ src/animation_catalog.cpp
@@ -4,12 +4,16 @@
 #include "custom_animations.h"
 
 const Animation kPlaylist[] = {
+  {glow_allArray, glow_frames_count},
   {sleepy_allArray, sleepy_frames_count},
   {love_allArray, love_frames_count},
+  {headlights_allArray, headlights_frames_count},
+  {greeting_allArray, greeting_frames_count},
+  {bored_allArray, bored_frames_count},
   {evil_allArray, evil_frames_count},
+  {rev_allArray, rev_frames_count},
   {yawn_allArray, yawn_frames_count},
   {playful_allArray, playful_frames_count},
-  {greeting_allArray, greeting_frames_count},
   {pingpong_allArray, pingpong_frames_count}
 };
 
